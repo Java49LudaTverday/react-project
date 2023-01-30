8acbd0f79163308fc5328c0f60eb6c8c554561bb
LudaTverday <Ludachka22@gmail.com>
2023-01-30 23:57:36 +0200

    nextStep functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { getRandomDate } from './utils/random';
+import { Life } from './components/Life';
+import lifeConfig from './config/life-game.json';
+import './App.css'
 
 
 function App() {
   
-  return <div>
-    
+  return <div style={{display:"flex", justifyContent: "center", alignItems:"center"}}>
+    <Life dimentions={lifeConfig.dimentions} ticInterval={lifeConfig.ticInterval}></Life>
   </div>
 }
 
--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,22 +1,31 @@
 export function getRandomNumber(minValue: number, maxValue: number, isMinInclusive: boolean = true, isMaxInclusive: boolean = false): number {
+    
+     if (!isMinInclusive) {
+        minValue += 1;
+    } 
+     if (isMaxInclusive) {
+        maxValue += 1;
+    }    
+     if (minValue == maxValue ) {
+         throw "min may not be equaled to max";
+    }
     if (minValue > maxValue) {
         [minValue, maxValue] = [maxValue, minValue];
-    } else if (minValue == maxValue && (!isMinInclusive || !isMaxInclusive)) {
-         throw "min may not be equaled to max";
-    } else if (minValue + 1 === maxValue && !isMinInclusive && !isMaxInclusive) {
-         throw "min or max must be inclusive";
-    } else if (!isMinInclusive && !isMaxInclusive) {
-        minValue += 1;
-    } else if (isMinInclusive && isMaxInclusive) {
-        maxValue += 1;
-    } else if (!isMinInclusive && isMaxInclusive) {
-        minValue += 1; maxValue += 1;
-    }
-
-    return minValue + Math.floor(Math.random() * (maxValue - minValue));
+    } 
+    return minValue + Math.trunc(Math.random() * (maxValue - minValue));
 }
+
 export function getRandomMatrix(rows: number, columns: number, min: number, max: number): number[][] {
-    let rowsNumbers: number[][] = [];
+    const resMatrix: number[][] = [[]];
+    for (let i = 0; i < rows; i++) {
+        resMatrix[i] = [];
+        for (let j = 0; j < columns; j++) {
+            resMatrix[i][j] = getRandomNumber(min, max, true, true);
+        }
+    }
+    return resMatrix;   
+   
+   /* let rowsNumbers: number[][] = [];
     let columnsNumbers: number[] = [];
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < columns; j++) {
@@ -31,18 +40,12 @@ export function getRandomMatrix(rows: number, columns: number, min: number, max:
     // for (let a = 0; a < rowsNumbers.length; a++) {
     //     console.log(rowsNumbers[a])
     // }
-    return rowsNumbers;
+    return rowsNumbers;*/
 }
-export function getRandomArrayElement(array: any[]): any {
-    return array[getRandomNumber(0, array.length, true, false)];
+export function getRandomArrayElement<T>(array: T[]): T {
+    return array[getRandomNumber(0, array.length)];
 }
 export function getRandomDate(minYear: number, maxYear: number) {
-    if (minYear < 0) {
-        minYear *= -1;
-    }
-    if (maxYear < 0) {
-        maxYear *= -1;
-    }
     const year: number = getRandomNumber(minYear, maxYear);
     const month: number = getRandomNumber(0, 12);
     const day: number = getRandomNumber(0, 32);
