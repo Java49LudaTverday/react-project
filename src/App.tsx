163d3177cf8db22e32d3258b0c754208d82fa0b2
LudaTverday <Ludachka22@gmail.com>
2023-02-07 12:32:47 +0200

    authenticated User authorization

--- src/App.tsx
+++ src/App.tsx
@@ -10,19 +10,18 @@ import { Input } from './components/Input';
 
 function App() {
   const auth: boolean = useSelector<any, boolean>(state => state.auth.authenticated);
-  const authAdmin = useSelector<any, boolean> (state => state.auth.authorization);
+  const authAdmin = useSelector<any, boolean>(state => state.auth.authorization);
   const [operand, setOperand] = React.useState(1);
   const [factor, setFactor] = React.useState(10);
-   const [userName, setUserName] = React.useState('');
-  //setUserName(useSelector<any,string>(state=> state.auth.authenticated));
+  const [userName, setUserName] = React.useState('');
   return <div style={{ textAlign: "center" }}>
     {auth && <p>User: {userName}</p>}
     {authAdmin && <div>
-      <Input placeholder='Enter operand'  inputProcess={function (value: string): string {
+      <Input placeholder='Enter operand' inputProcess={function (value: string): string {
         setOperand(+value);
         return '';
       }}></Input>
-      <Input placeholder='Enter factor'  inputProcess={function (value: string): string {
+      <Input placeholder='Enter factor' inputProcess={function (value: string): string {
         setFactor(+value);
         return '';
       }}></Input>
@@ -35,8 +34,8 @@ function App() {
     {auth && <Logout></Logout>}
     {!auth && <div>
       <Login inputProcess={function (value: string): void {
-       setUserName(value);
-      } } ></Login>
+        setUserName(value);
+      }} ></Login>
     </div>}
   </div>
 }
