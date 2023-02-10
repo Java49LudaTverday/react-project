c65105f0c4c973fbd59c4f76a13e2da65e526aee
LudaTverday <Ludachka22@gmail.com>
2023-02-10 09:54:32 +0200

    modified Navigator.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
-import { Layout } from './components/navigators/Layout';
-import { Products } from './components/navigators/Products';
+import './App.css';
 import { BreadProducts } from './components/pages/BreadProducts';
 import { Customers } from './components/pages/Customers';
 import { DairyProducts } from './components/pages/DairyProducts';
 import { Home } from './components/pages/Home';
 import { Orders } from './components/pages/Orders';
-
+import { Navigator } from './components/navigators/Navigator'
+import { layoutConfig } from './models/layout-config';
+import { productsConfig } from './models/products-config';
 
 function App() {
 
   return <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Layout />}>
+      <Route path='/' element={<Navigator config={layoutConfig} />}>
         <Route index element={<Home/>}></Route>
         <Route path='customers' element={<Customers/>}/>
         <Route path='orders' element={<Orders/>}></Route>
-        <Route path='products' element={<Products/>}>
+        <Route path='products' element={<Navigator config={productsConfig}/>}>
           <Route path='dairy' element={<DairyProducts/>}/>
           <Route path='bread' element={<BreadProducts/>}/>
         </Route>
@@ -32,9 +31,3 @@ export default App;
 
 
 
-//// version checking  userAdmin///
-      // const arrUserName: string[] = state.authenticated.split('-');  //when we use a separator "-"
-      // const endUserName = userName.slice(-ADMIN.length, userName.length); //when we use just a word "admin"
-      // //if (arrUserName[1] == ADMIN) {
-      //   if (endUserName == ADMIN) {
-      //   authAdmin = true;
